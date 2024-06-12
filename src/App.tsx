ed73f058b55ef4192abe983366045079a6f86ef7
Brock Smedley <2791467+zeroXbrock@users.noreply.github.com>
2024-06-12 14:37:02 -0400

    add project

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,55 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+import { type TransactionRequestSuave, getSuaveWallet, getSuaveProvider } from "@flashbots/suave-viem/chains/utils"
+import { Hex, custom, http, stringToHex } from '@flashbots/suave-viem'
+
 function App() {
-  const [count, setCount] = useState(0)
+  const [wallet, setWallet] = useState<ReturnType<typeof getSuaveWallet>>()
+  const [userInput, setUserInput] = useState<string>()
+  const [toggled, setToggled] = useState(false)
+
+  useEffect(() => {
+    const suaveProvider = getSuaveProvider(http("https://localhost:8545"))
+    console.log(suaveProvider.chain)
+    const load = async () => {
+      if ('ethereum' in window && !wallet) {
+        console.log('ethereum is available')
+        // request accounts from window.ethereum
+        const eth = window.ethereum as any
+        const accounts = await eth.request({ method: 'eth_requestAccounts' });
+        console.log(accounts)
+        const wallet = getSuaveWallet({
+          transport: custom(eth),
+          jsonRpcAccount: accounts[0],
+        });
+        console.log(wallet)
+        setWallet(wallet)
+      } else {
+        console.log('ethereum is not available')
+      }
+    }
+    load()
+  }, [wallet])
+
+  const doTxThing = async () => {
+    console.log("doing a thing")
+    const suaveTx: TransactionRequestSuave = {
+      to: "0x0000000000000000000000000000000000000000",
+      value: 1n,
+      gasPrice: 1n,
+      gas: 1n,
+      type: "0x43",
+      data: stringToHex(userInput || "0x"),
+      confidentialInputs: "0x",
+      kettleAddress: "0xb5feafbdd752ad52afb7e1bd2e40432a485bbb7f",
+    }
+    wallet?.signTransaction(suaveTx).then((tx: Hex) => {
+      console.log(tx)
+    })
+  }
 
   return (
     <>
@@ -18,13 +63,21 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+        <button type="button" onClick={doTxThing}>
+          Do a thing
         </button>
+        <input disabled={!toggled} type="text" value={userInput} onChange={(e) => setUserInput(e.target.value)} placeholder='enter something!'/>
+        <div>
+          {userInput && <p>{userInput}</p>}
+        </div>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
+      <button type="button" onClick={() => setToggled(!toggled)}>
+        Toggle Me
+      </button>
+      {toggled ? <p>Toggle me on and off</p> : <p>NOT TOGGLED ANYMORE</p>}
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
